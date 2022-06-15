6d50750571fe5ac77c0c78b74d09e877e729f8e2
Miguel Arriaga Velasco <marriagav@fb.com>
2022-06-15 12:40:31 -0700

    fixed views

--- Flixter/Flixter/Flixter/MovieViewController.m
+++ Flixter/Flixter/Flixter/MovieViewController.m
@@ -1,23 +1,23 @@
 //
-//  ViewController.m
+//  MovieViewController.m
 //  Flixter
 //
 //  Created by Miguel Arriaga Velasco on 6/15/22.
 //
 
-#import "ViewController.h"
+#import "MovieViewController.h"
 
-@interface ViewController ()
-@property (nonatomic, strong) NSArray *myArray;
-
+@interface MovieViewController ()
+@property (nonatomic, strong) NSArray *movieArray;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
 
-@implementation ViewController
+@implementation MovieViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=0e516a013120b69038b03abfe1fa6e5e"];
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
@@ -30,19 +30,25 @@
                // TODO: Get the array of movies
                // TODO: Store the movies in a property to use elsewhere
                // TODO: Reload your table view data
-               // log an object with the %@ formatter.
-               NSLog(@"%@", dataDictionary);
-               self.myArray = dataDictionary[@"keyForAnArray"];
-               
-               for (id object in self.myArray) {
-                   NSLog(@"%@", object);
+               NSLog(@"%@", dataDictionary);// log an object with the %@ formatter.
+               self.movieArray = dataDictionary[@"keyForAnArray"];
+               for (id movie in self.movieArray) {
+                   NSLog(@"%@", movie);
                }
                
            }
        }];
     [task resume];
-    
 }
 
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
