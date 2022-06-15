ed31fb82781a00592779b0bf91d3ac551a7da8d4
Miguel Arriaga Velasco <marriagav@fb.com>
2022-06-15 14:17:22 -0700

    personalized cells

--- Flixter/Flixter/Flixter/View Controllers/MovieViewController.m
+++ Flixter/Flixter/Flixter/View Controllers/MovieViewController.m
@@ -7,7 +7,7 @@
 
 #import "MovieViewController.h"
 
-@interface MovieViewController ()
+@interface MovieViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (nonatomic, strong) NSArray *movieArray;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.tableView.dataSource=self;
+    self.tableView.delegate=self;
+    
+    
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -27,18 +32,35 @@
            else {
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
 
+               
                // TODO: Get the array of movies
                // TODO: Store the movies in a property to use elsewhere
                // TODO: Reload your table view data
-               NSLog(@"%@", dataDictionary);// log an object with the %@ formatter.
-               self.movieArray = dataDictionary[@"keyForAnArray"];
+               // NSLog(@"%@", dataDictionary);// log an object with the %@ formatter.
+               self.movieArray = dataDictionary[@"results"];
                for (id movie in self.movieArray) {
                    NSLog(@"%@", movie);
                }
+               [self.tableView reloadData];
                
            }
        }];
     [task resume];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:
+(NSInteger)section{
+    return self.movieArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:
+    (NSIndexPath *)indexPath{
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    
+    NSDictionary *movie = self.movieArray[indexPath.row];
+    cell.textLabel.text = movie[@"title"];
+    
+    return cell;
 }
 
 /*
